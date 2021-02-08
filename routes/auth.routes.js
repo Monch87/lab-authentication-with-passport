d2f605167e694c267c83999016fcc0e8fb57bc7a
montse <montserrat.mosqueda@gmail.com>
2021-02-08 16:16:26 +0100

    done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,50 @@
 const express = require('express')
 const router = express.Router()
 // Require user model
+const passport = require('passport')
+const User = require('../models/user.model')
 
 // Add bcrypt to encrypt passwords
+const bcrypt = require('bcrypt')
+const bcryptSalt = 10
 
-// Add passport
+router.get('/signup', (req, res) => res.render('auth/signup')) 
+router.post('/signup', (req, res, next) => {
+	const {username, password} = req.body
+	if(!username || !password) {
+		res.render('auth/signup',{errorMsg: 'Fields required'})
+	}
+	User
+	.findOne({username})
+	.then(user => {
+		if (user) {
+			res.render('auth/signup', { errorMsg: 'Username already exists' })
+			return
+		}
+		const salt = bcrypt.genSaltSync(bcryptSalt)
+		const hashPass = bcrypt.hashSync(password, salt)
+		User
+			.create({ username, password: hashPass })
+			.then(() => res.redirect('/'))
+				.catch(() => res.render('auth/signup', { errorMsg: 'Error de servidor' }))
+	})
+	.catch(error => next(new Error(error)))
+}) 
 
-const ensureLogin = require('connect-ensure-login')
 
-router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
-	res.render('auth/private', { user: req.user })
+router.get('/login', (req, res) => res.render('auth/login')) 
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+	passReqToCallback: true
+}))
+
+
+router.get('/logout', (req, res) => {
+    req.logout();
+res.redirect('/');
 })
+
 
 module.exports = router
--- routes/base.routes.js
+++ routes/base.routes.js
@@ -4,5 +4,11 @@ const router = express.Router()
 // Endpoints
 router.get('/', (req, res) => res.render('index'))
 
+const ensureLogin = require('connect-ensure-login')
+
+router.get('/profile', ensureLogin.ensureLoggedIn(), (req, res) => {
+    res.render('auth/private', { user: req.user })
+})
+
 
 module.exports = router
